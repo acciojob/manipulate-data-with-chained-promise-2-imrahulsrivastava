ffe2ad4e618b920810359d8e4e356dc660735be3
Rahul Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-09-23 16:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+function manipulateData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const arr = [1, 2, 3, 4];
+      resolve(arr);
+    }, 3000);
+  });
+}
+
+manipulateData()
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evenNumbers = arr.filter((num) => num % 2 === 0);
+        document.getElementById(
+          "output"
+        ).innerText = `Even Numbers: ${evenNumbers.join(", ")}`;
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const multipliedNumbers = evenNumbers.map((num) => num * 2);
+        document.getElementById(
+          "output"
+        ).innerText = `Multiplied Numbers: ${multipliedNumbers.join(", ")}`;
+        resolve(multipliedNumbers);
+      }, 2000);
+    });
+  })
+  .catch((error) => {
+    document.getElementById("output").innerText = `Error: ${error}`;
+  });
